23438977c4cb1faba068c183c3854036e09a30a3
= <rtyenriques@gmail.com>
2022-01-05 00:31:09 -0800

    commit tree 5664ac1329b98968edba70b76dc1754422abe02d

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+// const findMatching = () => {
+//     drivers.filter(n => {
+//         return n === "Bobby"
+//     })
+// }
+
+function findMatching(array, arg) {
+
+    let newArray = array.filter(n=> {
+    n = n.toLowerCase();
+     return n == arg.toLowerCase();
+      
+         
+    })
+ 
+    return newArray
+}
+
+// let findMatching = () => drivers.filter(n=> {
+//             return n == 'Bobby';
+//         })
